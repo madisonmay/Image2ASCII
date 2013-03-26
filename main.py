4733a8bd7dd55192d3d5dfb3ebad481d2933fb5d
Bill Warner <billwarner13@gmail.com>
2013-03-26 12:25:04 -0400

    Removing trash

--- main.py
+++ main.py
@@ -7,27 +7,6 @@ import os
 
 asciiGroup = [' ', '.', '-', 'o', '*', '%', '#']
 
-def get_gray(s):
-   im = Image.open(s+'.jpg')
-   i = ImageOps.grayscale(im)
-   i.save(s+'-grayscale.jpg')
-   print('A grayscale version of your image is now saved as '+s+'-grayscale.jpg.')
-        
-def get_array(s):
-   im = Image.open(s+'.jpg')
-   imageW = im.size[0]
-   imageH = im.size[1]
-   rgb = []
-   for y in range(imageH):
-      rgb.append([])
-      for x in range(imageW):
-         xy = (x, y)
-         rgb[y].append(im.getpixel(xy))
-        #f = open(s+'-array.txt','w')
-        #f.write(str(rgb))
-        #f.close()
-   return rgb
-
 def delete_f():
    try:
       s=raw_input('Enter a file name: ')
@@ -105,30 +84,3 @@ if(__name__ == '__main__'):
    printAscii(pic)
    end = time.clock()
    print(end - start)
-
-
-'''
-def delete_f():
-   try: 
-      s=raw_input('Enter a file name: ')
-      os.remove(s)
-   except AttributeError: print('Input is not a valid file in this directory.')
-
-def test_f(arr):        
-   d = set()
-   dist = {}
-   for r in range(len(arr)):
-      for c in range(len(arr[r])):
-         d.add(str(arr[r][c]))
-         try:
-            dist[arr[r][c]]+=1
-         except KeyError:
-            dist[arr[r][c]]=1
-   print(d)
-   print(len(d))
-   test = []
-   for k,v in dist.items():
-      test.append((v,k))
-   test.sort()
-   print(test)
-'''
