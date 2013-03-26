47750834f428acabf0d336e14de856c2e2dbd1ff
Bill Warner <billwarner13@gmail.com>
2013-03-26 11:25:58 -0400

    Removed unused methods

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # http://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
-# test comment for github
 from PIL import Image
 from PIL import ImageFilter
 from PIL import ImageOps
@@ -9,41 +8,11 @@ asciiGroup = [' ', '.', '-', 'o', '*', '%', '#']
 
 def menu():
    order = raw_input('?: ')
-   if (order=='grayify'):
-      grayify()
-      menu()
-   elif(order=='delete'):
+   if(order=='delete'):
       delete_f()
-      menu()
-   elif(order=='array'):
-      s=raw_input('Enter a file name: ')
-      get_array(s)
       menu()
    elif (order=='exit'): return
    else: menu()
-def grayify():
-   try: get_gray(s=raw_input('Enter a file name: '))
-   except IOError: print('Input is not a valid image in this directory.')
-def get_gray(s):
-   im = Image.open(s+'.jpg')
-   i = ImageOps.grayscale(im)
-   i.save(s+'-grayscale.jpg')
-   print('A grayscale version of your image is now saved as '+s+'-grayscale.jpg.')
-
-def get_array(s):
-   im = Image.open(s+'.jpg')
-   imageW = im.size[0]
-   imageH = im.size[1]
-   rgb = []
-   for y in range(imageH):
-      rgb.append([])
-      for x in range(imageW):
-         xy = (x, y)
-         rgb[y].append(im.getpixel(xy))
-        #f = open(s+'-array.txt','w')
-        #f.write(str(rgb))
-        #f.close()
-   return rgb
 
 def get_agray(s):
    im = Image.open(s+'.jpg')
@@ -100,34 +69,6 @@ def delete_f():
       os.remove(s)
    except AttributeError: print('Input is not a valid file in this directory.')
 
-def test_f(arr):
-   d = set()
-   dist = {}
-   for r in range(len(arr)):
-      for c in range(len(arr[r])):
-         d.add(str(arr[r][c]))
-         try:
-            dist[arr[r][c]]+=1
-         except KeyError:
-            dist[arr[r][c]]=1
-   print(d)
-   print(len(d))
-   test = []
-   for k,v in dist.items():
-      test.append((v,k))
-   test.sort()
-   print(test)
-
-
 #pic = get_agray('smiley')
 pic = scale('smiley', 20, 35)
 printAscii(pic)
-'''
-for row in pic:
-   printed = ''
-   for pixel in row:
-      if(pixel > 127):
-         printed = printed + ' '
-      else:
-         printed = printed + '#'
-   print(printed)
