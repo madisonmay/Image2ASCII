d2f97f3401d03b358d847999d799002476274a2a
Mitchell Kwock <m.t.kwock@gmail.com>
2013-03-30 19:32:27 -0400

    Added all character pictures for later use under the photos/characters folder.  All files are named by (unicode character number).png

--- char_gen.py
+++ char_gen.py
@@ -1,3 +1,16 @@
 #Character Picture Generator
+import Image
 
-for i in range(0,128-33):
+im = Image.open("photos/characters/chars.png")
+
+for l in range(33,128):
+	i = l - 33
+	box = (i * 9 + 1, 0, (i + 1) * 9 + 1, 18)
+	trim_char = im.crop(box)
+	trim_char.save("photos/characters/" + str(l-1) + ".png")
+
+for l in range(161,256):
+	i = l-161
+	box = (i * 9 + 1, 18, (i + 1) * 9 + 1, 36)
+	trim_char = im.crop(box)
+	trim_char.save("photos/characters/" + str(l) + ".png")
