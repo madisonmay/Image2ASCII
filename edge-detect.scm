c2b2e6387e76f04231abe90ccea37615063cecdb
madisonmay13 <madison.may@students.olin.edu>
2013-03-31 22:57:28 -0400

    Batch mode

--- edge-detect.scm
+++ edge-detect.scm
@@ -12,3 +12,18 @@
      ; (gimp-brightness-contrast drawable 0 contrast)
      (gimp-file-save RUN-NONINTERACTIVE image drawable "result.jpg" "result.jpg")
      (gimp-image-delete image)))
+
+(define (edge-batch pattern low high)
+(let* ((filelist (cadr (file-glob pattern 1))))
+(while (not (null? filelist))
+       (let* ((filename (car filelist))
+              (image (car (gimp-file-load RUN-NONINTERACTIVE
+                                          filename filename)))
+              (drawable (car (gimp-image-get-active-layer image))))
+        (plug-in-laplace RUN-NONINTERACTIVE image drawable)
+        (gimp-hue-saturation drawable 0 0.0 0.0 -100.0)
+        (gimp-threshold drawable low high)
+         (gimp-file-save RUN-NONINTERACTIVE
+                         image drawable filename filename)
+         (gimp-image-delete image))
+       (set! filelist (cdr filelist)))))
